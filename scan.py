fe58fa9e53f089dd7118000d813ee3d9fba71348
Lev Perschin <lev.perschin@gmail.com>
2017-07-10 12:07:39 +0200

    saving all information in scan member variables now, not returning them. fixes

--- scan.py
+++ scan.py
@@ -1,32 +1,35 @@
 import os.path
-import numpy as np
-import time
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 from skimage import filters, feature
 
 
 class Scan:
     def __init__(self):
-        self.__matrix = np.array([0])
+        self.matrix = []
+        self.peaks = []
+        self.polar_views = []
+        self.debug = False
+        self.ascan_size = 512
 
     def show_matrix(self):
         """Plot current state of the matrix."""
-        plt.figure(), plt.imshow(self.__matrix)
+        plt.figure(), plt.imshow(self.matrix)
 
     def get_matrix(self):
         """Get the current matrix returned."""
-        return self.__matrix
+        return self.matrix
 
     def load_data(self, file_name):
         """Load binary data into scaled matrix."""
         if os.path.isfile(file_name):
             f = open(file_name, "r")
             # Load file from binary file
-            self.__matrix = np.fromfile(f, np.float32, sep="")
+            self.matrix = np.fromfile(f, np.float32, sep="")
             # Reshape to matrix dimensions
-            self.__matrix = np.reshape(self.__matrix, (512, np.size(self.__matrix) // 512), order='F')
-            self.__matrix = self.__scale_interval_zero_one(self.__matrix)
+            self.matrix = np.reshape(self.matrix, (self.ascan_size, np.size(self.matrix) // self.ascan_size), order='F')
+            self.matrix = self.__scale_interval_zero_one(self.matrix)
             print("Loading of data succefully finished.")
         else:
             print("Error loading file.")
@@ -42,17 +45,17 @@ class Scan:
 
     def __remove_second_column(self):
         """Remove every second column of the matrix. Maybe never will be used."""
-        new_matrix = np.empty((np.shape(self.__matrix)[0], np.shape(self.__matrix)[1] // 2))
-        for i in range(0, np.shape(self.__matrix)[1]):
+        new_matrix = np.empty((np.shape(self.matrix)[0], np.shape(self.matrix)[1] // 2))
+        for i in range(0, np.shape(self.matrix)[1]):
             if i % 2 == 0:
-                new_matrix[:, (i - 1) // 2] = self.__matrix[:, i]
-        self.__matrix = new_matrix
+                new_matrix[:, (i - 1) // 2] = self.matrix[:, i]
+        self.matrix = new_matrix
 
-    def find_peaks(self, width=None, debug=False):
+    def find_peaks(self, width=None):
         """Find peaks from matrix showing a sinus curve."""
 
         if width is not None:
-            matrix = self.__matrix[:, 0:width]
+            matrix = self.matrix[:, 0:width]
 
         min_width = 850
         max_width = 1400
@@ -61,7 +64,7 @@ class Scan:
         skin_layer_med = filters.median(skin_layer_cut, np.ones([5, 5]))
         skin_layer = feature.canny(skin_layer_med, sigma=1)
 
-        if debug:
+        if self.debug:
             plt.figure(), plt.imshow(skin_layer_med)
             plt.figure(), plt.imshow(skin_layer)
             plt.figure(), plt.imshow(skin_layer)
@@ -89,31 +92,34 @@ class Scan:
                         peak_at = c
             peak_at = peaks[-1] + min_width + peak_at
             peaks.append(peak_at)
+
+        self.peaks = peaks
         print("Found peaks: " + str(peaks) + ". Searched until: " + str(width))
 
-        if debug:
+        # Plot vertical line, where peak was found.
+        if self.debug:
             for i in peaks:
                 skin_layer[:, i] = 1
             plt.figure(), plt.imshow(skin_layer)
-        return peaks
 
-    def show_polar(self, peaks):
+    def create_polar_views(self):
         """Create polar views, save and plot them."""
         polar_vec = []
-        length_peaks = len(peaks)
+        length_peaks = len(self.peaks)
         for i in range(0, length_peaks):
             if i + 1 < length_peaks:
-                matrix = self.__matrix[:, peaks[i]: peaks[i + 1]]
+                matrix = self.matrix[:, self.peaks[i]: self.peaks[i + 1]]
                 polar_matrix = np.empty([1024, 1024])
                 matrix_shape = np.shape(matrix)
 
                 for x in range(0, matrix_shape[1]):
                     for y in range(0, matrix_shape[0]):
-                        xp = round(y * math.cos(2 * x * math.pi / matrix_shape[1]) + 512)
-                        yp = round(y * math.sin(2 * x * math.pi / matrix_shape[1]) + 512)
+                        xp = round(y * math.cos(2 * x * math.pi / matrix_shape[1]) + self.ascan_size)
+                        yp = round(y * math.sin(2 * x * math.pi / matrix_shape[1]) + self.ascan_size)
                         polar_matrix[xp, yp] = matrix[y, x]
 
                 polar_vec.append(polar_matrix)
-                plt.figure(), plt.imshow(polar_matrix)
+                if self.debug:
+                    plt.figure(), plt.imshow(polar_matrix)
+        self.polar_views = polar_vec
         print("Succesfully saved all polar images.")
-        return polar_vec
--- show_ascan.py
+++ show_ascan.py
@@ -1,14 +1,12 @@
+import sys
 from scan import Scan
 import matplotlib.pyplot as plt
-import numpy as np
-import sys
-
 
 def main():
     scan = Scan()
     scan.load_data(sys.argv[1])
-    peaks = scan.find_peaks(10000)
-    scan.show_polar(peaks)
+    scan.find_peaks(10000)
+    scan.create_polar_views()
 
 
 if __name__ == "__main__":
